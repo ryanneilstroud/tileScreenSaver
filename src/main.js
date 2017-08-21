84ebfe186ad709e0633c8c8f58ec373296104b87
Ryan Neil Stroud <ryanneilstroud@Ryans-MacBook-Pro.local>
2017-08-22 01:15:06 +0800

    Reformatted some code, including minor refactoring.

--- src/main.js
+++ src/main.js
@@ -1,129 +1,126 @@
+let $ = jQuery
 
-var numberOfColumns = 5;
-var numberOfRows = 10;
+var numberOfColumns = 5
+var numberOfRows = 10
 
-var maxTileRefreshTime = 2000;
+var maxTileRefreshTime = 2000
 
-var tileInterval = setInterval(function(){
-	var index = Math.floor((Math.random() * calculateNumOfTiles()) + 1);
-	$(".tile:eq(" + index + ")").animate({backgroundColor: getRandomHexValue()}, 1000, "linear");
-}, getRandomNumber());
+setInterval(function () {
+  var index = Math.floor((Math.random() * calculateNumOfTiles()) + 1)
+  $('.tile:eq(' + index + ')').animate({backgroundColor: getRandomHexValue()}, 1000, 'linear')
+}, getRandomNumber())
 
-$("#max_tile_refresh_time").text(maxTileRefreshTime);
-$("#number_of_rows").text(numberOfRows);
-$("#number_of_columns").text(numberOfColumns);
+$('#max_tile_refresh_time').text(maxTileRefreshTime)
+$('#number_of_rows').text(numberOfRows)
+$('#number_of_columns').text(numberOfColumns)
 
-//control max tile refresh time
-$("#decrease_max_tile_refresh_time").click(function() {
-	if (maxTileRefreshTime > 100) {
-		maxTileRefreshTime -= 100;
-		$("#max_tile_refresh_time").text(maxTileRefreshTime);
-	}
-});
+// control max tile refresh time
+$('#decrease_max_tile_refresh_time').click(function () {
+  if (maxTileRefreshTime > 100) {
+    maxTileRefreshTime -= 100
+    $('#max_tile_refresh_time').text(maxTileRefreshTime)
+  }
+})
 
-$("#increase_max_tile_refresh_time").click(function() {
-	maxTileRefreshTime += 100;
-	$("#max_tile_refresh_time").text(maxTileRefreshTime);
-});
+$('#increase_max_tile_refresh_time').click(function () {
+  maxTileRefreshTime += 100
+  $('#max_tile_refresh_time').text(maxTileRefreshTime)
+})
 
-//control number of ROWS
-$("#decrease_number_of_rows").click(function() {
-	if (numberOfRows >= 1) {
-		numberOfRows -= 1;
-		$("#number_of_rows").text(numberOfRows);
-		$("div.tile").remove();
-		appendTiles();
-	}
-});
+// control number of ROWS
+$('#decrease_number_of_rows').click(function () {
+  if (numberOfRows >= 1) {
+    numberOfRows -= 1
+    updateLabelWithNumber('rows', numberOfRows)
+  }
+})
 
-$("#increase_number_of_rows").click(function() {
-	numberOfRows += 1;
-	$("#number_of_rows").text(numberOfRows);
-	$("div.tile").remove();
-	appendTiles();
-});
+$('#increase_number_of_rows').click(function () {
+  numberOfRows += 1
+  updateLabelWithNumber('rows', numberOfRows)
+})
 
-//control number of COLUMNS
-$("#decrease_number_of_columns").click(function() {
-	if (numberOfColumns >= 1) {
-		numberOfColumns -= 1;
-		$("#number_of_columns").text(numberOfColumns);
-		$("div.tile").remove();
-		appendTiles();
-	}
-});
+// control number of COLUMNS
+$('#decrease_number_of_columns').click(function () {
+  if (numberOfColumns >= 1) {
+    numberOfColumns -= 1
+    updateLabelWithNumber('columns', numberOfColumns)
+  }
+})
 
-$("#increase_number_of_columns").click(function() {
-	numberOfColumns += 1;
-	$("#number_of_columns").text(numberOfColumns);
-	$("div.tile").remove();
-	appendTiles();
-});
+$('#increase_number_of_columns').click(function () {
+  numberOfColumns += 1
+  updateLabelWithNumber('columns', numberOfColumns)
+})
 
-var setTileInterval = function(){
-    clearInterval(interval);
+var setTileInterval = function () {
+  clearInterval(interval)
 
-	var index = Math.floor((Math.random() * calculateNumOfTiles()) + 1);
-	$(".tile:eq(" + index + ")").animate({backgroundColor: getRandomHexValue()}, 1000, "linear");
+  var index = Math.floor((Math.random() * calculateNumOfTiles()) + 1)
+  $('.tile:eq(' + index + ')').animate({backgroundColor: getRandomHexValue()}, 1000, 'linear')
 
-    interval = setInterval(setTileInterval, getRandomNumber());
+  interval = setInterval(setTileInterval, getRandomNumber())
 }
-var interval = setInterval(setTileInterval, getRandomNumber());
+var interval = setInterval(setTileInterval, getRandomNumber())
 
-function calculateNumOfTiles() {
-	return numberOfColumns*numberOfRows;
+function updateLabelWithNumber (collection, number) {
+  $('#number_of_' + collection).text(number)
+  $('div.tile').remove()
+  appendTiles()
 }
 
-function getLength() {
-	return $(window).width()/numberOfColumns;
+function calculateNumOfTiles () {
+  return numberOfColumns * numberOfRows
 }
 
-function getRandomHexValue() {
-	return "#" + (Math.random()*0xFFFFFF<<0).toString(16);
+// get tile width
+function getLength () {
+  return $(window).width() / numberOfColumns
 }
 
-function appendTiles() {
-	for(var i = 1; i <= calculateNumOfTiles(); i++) {
-		$('body').append('<div class="tile square-tile" style="width:' + getLength() + 'px; height:' + getLength() + 'px; background-color:' + getRandomHexValue() + '"></div>');
-	}
+function getRandomHexValue () {
+  return '#' + (Math.random() * 0xFFFFFF << 0).toString(16)
 }
 
-function updateTiles() {
-	for(var i = 0; i <= calculateNumOfTiles(); i++) {
-		$(".tile:eq(" + i + ")").css({"height":getLength(),"width":getLength()});
-	}
+function appendTiles () {
+  for (var i = 1; i <= calculateNumOfTiles(); i++) {
+    $('body').append('<div class="tile square-tile" style="width:' + getLength() + 'px; height:' + getLength() + 'px; background-color:' + getRandomHexValue() + '"></div>')
+  }
 }
 
-function getRandomNumber() {
-	var t = Math.floor((Math.random() * maxTileRefreshTime));
-	console.log(t);
-	return t;
+function updateTiles () {
+  for (var i = 0; i <= calculateNumOfTiles(); i++) {
+    $('.tile:eq(' + i + ')').css({'height': getLength(), 'width': getLength()})
+  }
 }
 
-$('.toggle-settings').click(function() {
-  $('#settings_panel').fadeToggle();
-});
+function getRandomNumber () {
+  let t = Math.floor((Math.random() * maxTileRefreshTime))
+  return t
+}
 
-$('.tile-shape').click(function() {
+$('.toggle-settings').click(function () {
+  $('#settings_panel').fadeToggle()
+})
 
-	if ($('i.tile-shape').hasClass("fa-circle-o") == true) {
-		$('i.tile-shape').removeClass("fa-circle-o").addClass("fa-square-o");
-		
-		$(".tile").each(function(index) {
-			$(".tile:eq(" + index + ")").removeClass("tile-shape-square").addClass("tile-shape-circle");
-  		});
-	} else {
-		$("i.tile-shape").removeClass("fa-square-o").addClass("fa-circle-o");
+$('.tile-shape').click(function () {
+  if ($('i.tile-shape').hasClass('fa-circle-o') === true) {
+    $('i.tile-shape').removeClass('fa-circle-o').addClass('fa-square-o')
 
-		$(".tile").each(function(index) {
-			$(".tile:eq(" + index + ")").removeClass("tile-shape-circle").addClass("tile-shape-square");
-   		});
-	}
+    $('.tile').each(function (index) {
+      $('.tile:eq(' + index + ')').removeClass('tile-shape-square').addClass('tile-shape-circle')
+    })
+  } else {
+    $('i.tile-shape').removeClass('fa-square-o').addClass('fa-circle-o')
 
-});
+    $('.tile').each(function (index) {
+      $('.tile:eq(' + index + ')').removeClass('tile-shape-circle').addClass('tile-shape-square')
+    })
+  }
+})
 
-$( window ).resize(function() {
-	updateTiles();
-});
+$(window).resize(function () {
+  updateTiles()
+})
 
-appendTiles();
+appendTiles()
